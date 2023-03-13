179bd04c4e1059281c6dca2e53f0df8df622423f
Johnsmz <88699538+Johnsmz@users.noreply.github.com>
2023-03-13 05:48:15 +0000

    Update C-Prebuild.h

--- C-Prebuild.h
+++ C-Prebuild.h
@@ -11,7 +11,7 @@ typedef struct{int* arr;int length;} intArray;
 
 typedef struct{double* arr;int length;} doubleArray;
 
-float power(float num, float n){int out=1; for(int i = 0; i < n; i++){out *= num;}return out;}
+float power(float num, float n){if(n<0){num = 1/num;}float out=1; for(int i = 0; i < n; i++){out *= num;}return out;}
 
 char* clonestring(char* str){char out[strlen(str)];strcpy(out, str);char* ret = out; return ret;}
 
@@ -27,7 +27,7 @@ char itoc(int in){if(in<0||in>9){return ' ';}return (char)(in+48);}
 
 char* ita(int in){int a = digit(in);char arr[a];for(int i = 0; i < a; i++){arr[a-i-1]=itoc(in%10);in/=10;}char* ret = arr;return ret;}
 
-char* ftoa(float in, int dec){if(dec<0){return "";}if((int)(in*power(10,dec+1))%10>=5){in+=power(10, -dec);}int a = digit((int)in);char out[dec+1+a];char*temp = ita((int)in);for(int i = 0; i < a; i++){out[i]=temp[i];};out[a]='.';for(int i = 1; i <=dec; i++){in*=10;out[a+i]=itoc((int)in%10);};char* ret = out;return ret;}
+char* ftoa(float in, int dec){if(dec<0){return "";}if((int)(in*power(10.0,dec+1.0))%10>=5){in+=power(10.0, dec*(-1.0));}int a = digit((int)in);char out[dec+1+a];char* temp = ita((int)in);for(int i = 0; i < a; i++){out[i]=temp[i];};out[a]='.';for(int i = 1; i <=dec; i++){in*=10;out[a+i]=itoc((int)in%10);}char* ret = out;return ret;}
 
 int transferNum(char a){if((int)a>=97&&(int)a<=122){return (int)a-87;}return (int)a-48;}
 
